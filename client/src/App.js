589f09278763e96281bbd3b770aa0b06d94f4492
Hongjing <hongjingdeng@gmail.com>
2020-10-28 22:28:04 -0400

    initial template

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+        <Route exact path={"/signup"}>
+            <Books />
+          </Route>
+          <Route exact path={"/login"}>
+            <Books />
+          </Route>
+          <Route exact path={["/", "/books"]}>
+            <Books />
+          </Route>
+          <Route exact path="/books/:bookid">
+            <Detail />
+          </Route>
+          <Route>
+            <NoMatch />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
