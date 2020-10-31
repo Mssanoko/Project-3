ae573e7834a33ba87c2e4a47bd27404d1ca276cf
Hongjing <hongjingdeng@gmail.com>
2020-10-31 12:31:12 -0400

    fixing routes and export router

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const bookRoutes = require("./books");
-
+const loginRoutes = require("./login");
 // Book routes
 router.use("/books", bookRoutes);
-
+router.use("/user", loginRoutes);
 module.exports = router;
