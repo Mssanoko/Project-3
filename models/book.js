9e866fa6cd0d293316f99f4ac0f5466a30210dea
Hongjing <hongjingdeng@gmail.com>
2020-11-06 20:48:27 -0500

    connecting userInput to get translated from backend api call

--- models/book.js
+++ models/book.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-  title: { type: String, required: true },
+  phrase: { type: String, required: true },
   author: { type: String, required: true },
   synopsis: String,
   date: { type: Date, default: Date.now }
