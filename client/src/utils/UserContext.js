3ac7cc4b81f50f9359fdd95e85b4a73320e4e5d4
Sarah Abouelela <abouelelas@gmail.com>
2020-11-07 22:35:57 -0500

    pull changes

--- client/src/utils/UserContext.js
+++ client/src/utils/UserContext.js
@@ -3,7 +3,7 @@ import {createContext} from 'react';
 const UserContext = createContext({
     email: "",
     setEmail: () => {},
-    loggedIn: true,
+    loggedIn: false,
     setLoggedIn: () => {}
 });
 
