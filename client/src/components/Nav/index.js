5ce94a5035cd3626e8b3a1a24b868744129c63ef
Sarah Abouelela <abouelelas@gmail.com>
2020-11-08 19:01:03 -0500

    created initial readme

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -10,7 +10,7 @@ function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <a className="navbar-brand" href="/">
-        VOCABULARY-BEE
+       Vocabulary-Bee
       </a>
       { (() => {
         if(loggedIn){
