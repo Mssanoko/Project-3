75c456a387c1e8d8d530350265d286c92ddf7a16
Sarah Abouelela <abouelelas@gmail.com>
2020-11-09 22:38:27 -0500

    npm run build

---
+++ client/build/precache-manifest.6d0416fa42a74c4d8ca5d5c129ef1f08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01049f3615c187b21ce76cf46b1db9ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65db4163543ad874b7f1",
+    "url": "/static/css/main.8acad0f7.chunk.css"
+  },
+  {
+    "revision": "e4d8acbf029c83830026",
+    "url": "/static/js/2.04cc6d68.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.04cc6d68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65db4163543ad874b7f1",
+    "url": "/static/js/main.64700236.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
