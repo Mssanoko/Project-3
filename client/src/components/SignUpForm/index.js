7d5ca2c115ad2cee3667afca39c03c1ed721675a
Sarah Abouelela <abouelelas@gmail.com>
2020-11-09 19:47:34 -0500

    vdev debug

--- client/src/components/SignUpForm/index.js
+++ client/src/components/SignUpForm/index.js
@@ -4,7 +4,7 @@ import API from '../../utils/API';
 import UserContext from '../../utils/UserContext';
 
 function SignUpForm(props) {
-    const { email, setEmail, loggedIn, setLoggedIn } = useContext(UserContext);
+    const { email, setEmail, loggedIn, setLoggedIn, name, setName } = useContext(UserContext);
     const emailInput = useRef();
     const passwordInput = useRef();
     let extraProps = {}
@@ -14,6 +14,7 @@ function SignUpForm(props) {
     let emailId = props.className ? props.className + "-signup-email" : "signup-email";
     let emailHelpId = props.className ? props.className + "-signup-email-help" : "signup-email-help";
     let passwordId = props.className ? props.className + "-signup-password" : "signup-password";
+    let nameId = props.className ? props.className + "-signup-name" : "signup-name";
     const handleSubmit = event => {
         // if the user hits enter or hits the button, this function will fire
         event.preventDefault();
@@ -26,11 +27,12 @@ function SignUpForm(props) {
         // .catch(err => {
         //     console.log(err);
         // });
-        API.signup({ email: emailInput.current.value, password: passwordInput.current.value})
+        API.signup({ email: emailInput.current.value, password: passwordInput.current.value, name: nameInput.current.value})
             .then(data => {
                 // console.log(data);
                 setEmail(data.data.email);
                 setLoggedIn(true);
+                setName(data.data.name);
             })
             .catch(err => {
                 console.log(err);
@@ -41,6 +43,10 @@ function SignUpForm(props) {
             { (() => {
                 if (!loggedIn) {
                     return (<form {...extraProps} onSubmit={handleSubmit}>
+                         <div className="form-group">
+                            <label htmlFor={nameId}>Name</label>
+                            <input ref={nameInput} type="text" className="form-control" id={nameId} />
+                        </div>
                         <div className="form-group">
                             <label htmlFor={emailId}>Email address</label>
                             <input ref={emailInput} type="email" className="form-control" id={emailId} aria-describedby={emailHelpId} />
